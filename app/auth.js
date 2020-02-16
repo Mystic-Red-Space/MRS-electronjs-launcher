2989d2c90875a05c52b3bd7d3b2654f051c87ebe
Coder-Iro <doohee006@gmail.com>
2020-02-17 03:47:00 +0900

    change http request module to axios

--- app/auth.js
+++ app/auth.js
@@ -1,14 +1,9 @@
-const http = require('https');
+const axios = require('axios');
 const uuid = require('uuid4');
 
 function login(id,password) {
-  const options = {
-    hostname: "https://authserver.mojang.com",
-    path: "/authenticate",
-    method: "POST",
-    headers: {
-      'Content-type': 'application/json'
-    }
-  };
-  var req = http.request(options, )
+  let clientToken = uuid()
+  axios.post('https://authserver.mojang.com/authenticate', {
+
+  });
 }
