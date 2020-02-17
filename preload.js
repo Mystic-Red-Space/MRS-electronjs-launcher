355e36e3fc730af71b8957328b672ca3b58d887b
Coder-Iro <doohee006@gmail.com>
2020-02-18 02:22:06 +0900

    Optimize event listener

--- preload.js
+++ preload.js
@@ -2,9 +2,10 @@
 // It has the same sandbox as a Chrome extension.
 const mclogin = require("./auth");
 window.onload = function () {
-    document.getElementById("btnLogin").onclick = function () {
+    function login() {
         mclogin.login(document.getElementById("idbox").value, document.getElementById("passbox").value).then((res) => {
             console.log(JSON.stringify(res.data))
-        })
+        });
+        document.getElementById("btnLogin").onclick = login;
     }
 };
