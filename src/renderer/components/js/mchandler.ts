73315c889eaee8073529111c6a2502f4dfe2081b
Coder-Iro <doohee006@gmail.com>
2020-02-28 04:53:32 +0900

    I don't know what I did 3

--- src/renderer/components/js/mchandler.ts
+++ src/renderer/components/js/mchandler.ts
@@ -1,11 +1,12 @@
 import {Installer} from "@xmcl/installer";
 import {MinecraftLocation} from "@xmcl/core";
 
-async function testinstall() {
+
+async function testlaunch() {
     const minecraft: MinecraftLocation = "./minecraft";
     const list: Installer.VersionList = await Installer.getVersionList();
     const aVersion: Installer.Version = list[0]; // i just pick the first version in list here
     await Installer.install("client", aVersion, minecraft);
 }
 
-testinstall();
+testlaunch();
